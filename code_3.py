0dac5d37b7a48cb08a82bc5b9bc388ca48d91a95
genfilweb <142978168+genfilweb@users.noreply.github.com>
2023-12-06 09:18:20 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,39 @@
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":
+
+    for item in data:
+
+    data = generate_random_data()
+    main()
+import random
+    return data
+def main():
+        print(f"Random Number: {item}")
+        int[] data = new int[10];
+    }
+            System.out.println("Random Number: " + item);
+        }
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+}    return data
+
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+    for item in data:
+        print(f"Random Number: {item}")
+def main():
+
+
+    main()
+import random
+    data = generate_random_data()
+
+def generate_random_data():
